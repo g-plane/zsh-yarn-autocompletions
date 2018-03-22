84bd937970cfe58e8719e47bdd5ce4e1597f4223
Pig Fang <g-plane@hotmail.com>
2018-03-22 19:58:05 +0800

    feat(deps): support custom dependencies autocompletion

--- src/deps.rs
+++ src/deps.rs
@@ -1,15 +1,24 @@
 use std::env;
 use std::fs::File;
+use std::path::PathBuf;
 use std::collections::HashMap;
 
 extern crate serde;
 extern crate serde_json;
+extern crate serde_yaml;
 
 #[allow(non_snake_case)]
 #[derive(Deserialize)]
 struct Pkg {
     dependencies: Option<HashMap<String, String>>,
     devDependencies: Option<HashMap<String, String>>,
+}
+
+#[derive(Deserialize)]
+struct UserCustomDeps {
+    dependencies: Option<Vec<String>>,
+    dev_dependencies: Option<Vec<String>>,
+    exclude: Option<Vec<String>>,
 }
 
 pub fn fetch_installed_packages() -> String {
@@ -57,14 +66,108 @@ pub fn fetch_installed_packages() -> String {
     }
 }
 
-pub fn return_dependencies() -> String {
+pub fn return_dependencies(path: Option<PathBuf>) -> String {
+    let path = match path {
+        Some(path) => path,
+        None => default_custom_deps_file_path()
+    };
+
     let dependencies = vec!["vue", "vuex"];
+    let mut dependencies: Vec<String> = dependencies
+        .iter()
+        .map(|&dep| String::from(dep))
+        .collect();
+    let custom = fetch_custom_dependencies(&path);
+    dependencies.extend(custom);
+    
+    let exclude = fetch_exclude_dependencies(&path);
+    let dependencies: Vec<String> = dependencies
+        .into_iter()
+        .filter(|dep| !exclude.contains(dep))
+        .collect();
+
     dependencies.join("\n")
 }
 
-pub fn return_dev_dependencies() -> String {
-    let dev_dependencies = vec!["typescript", "eslint", "eslint-config-gplane"];
+pub fn return_dev_dependencies(path: Option<PathBuf>) -> String {
+    let path = match path {
+        Some(path) => path,
+        None => default_custom_deps_file_path()
+    };
+
+    let dev_dependencies = vec!["typescript", "eslint"];
+    let mut dev_dependencies: Vec<String> = dev_dependencies
+        .iter()
+        .map(|&dep| String::from(dep))
+        .collect();
+    let custom = fetch_custom_dev_dependencies(&path);
+    dev_dependencies.extend(custom);
+
+    let exclude = fetch_exclude_dependencies(&path);
+    let dev_dependencies: Vec<String> = dev_dependencies
+        .into_iter()
+        .filter(|dep| !exclude.contains(dep))
+        .collect();
+
     dev_dependencies.join("\n")
+}
+
+fn default_custom_deps_file_path() -> PathBuf {
+    match env::home_dir() {
+        Some(path) => path.join(".yarn-autocompletions.yml"),
+        None => PathBuf::new()
+    }
+}
+
+fn fetch_custom_dependencies(path: &PathBuf) -> Vec<String> {
+    if let Ok(file) = File::open(path) {
+        match serde_yaml::from_reader(file) {
+            Ok(custom) => {
+                let custom: UserCustomDeps = custom;
+                match custom.dependencies {
+                    Some(deps) => deps,
+                    None => vec![]
+                }
+            }
+            Err(_) => vec![]
+        }
+    } else {
+        vec![]
+    }
+}
+
+fn fetch_custom_dev_dependencies(path: &PathBuf) -> Vec<String> {
+    if let Ok(file) = File::open(path) {
+        match serde_yaml::from_reader(file) {
+            Ok(custom) => {
+                let custom: UserCustomDeps = custom;
+                match custom.dev_dependencies {
+                    Some(deps) => deps,
+                    None => vec![]
+                }
+            }
+            Err(_) => vec![]
+        }
+    } else {
+        vec![]
+    }
+}
+
+fn fetch_exclude_dependencies(path: &PathBuf) -> Vec<String> {
+    if let Ok(file) = File::open(path) {
+        match serde_yaml::from_reader(file) {
+            Ok(custom) => {
+                let custom: UserCustomDeps = custom;
+                match custom.exclude {
+                    Some(deps) => deps,
+                    None => vec![]
+                }
+            }
+            Err(_) => vec![]
+        }
+    } else {
+        vec![]
+    }
 }
 
 #[test]
@@ -74,4 +177,24 @@ fn test_fetch_installed_packages() {
     let mut packages: Vec<&str> = output.split('\n').collect();
     packages.sort();
     assert_eq!(packages, ["a", "b", "c", "d"]);
+}
+
+#[test]
+fn test_return_dependencies() {
+    let path = PathBuf::from("yarn-autocompletions.example.yml");
+    let output = return_dependencies(Some(path));
+    let output = output.trim();
+    let mut packages: Vec<&str> = output.split('\n').collect();
+    packages.sort();
+    assert_eq!(packages, ["react", "react-dom", "vue"]);
+}
+
+#[test]
+fn test_return_dev_dependencies() {
+    let path = PathBuf::from("yarn-autocompletions.example.yml");
+    let output = return_dev_dependencies(Some(path));
+    let output = output.trim();
+    let mut packages: Vec<&str> = output.split('\n').collect();
+    packages.sort();
+    assert_eq!(packages, ["babel-core", "eslint", "webpack"]);
 }
--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     match args[1].as_str() {
         "scripts" => print!("{}", scripts::fetch_npm_scripts()),
-        "add" => println!("{}", deps::return_dependencies()),
-        "add-dev" => println!("{}", deps::return_dev_dependencies()),
+        "add" => println!("{}", deps::return_dependencies(None)),
+        "add-dev" => println!("{}", deps::return_dev_dependencies(None)),
         "remove" => print!("{}", deps::fetch_installed_packages()),
         _ => ()
     };
