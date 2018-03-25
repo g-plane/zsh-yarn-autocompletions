1b0a3b49eb464e33855b8f7467f66d73e7122ba0
Pig Fang <g-plane@hotmail.com>
2018-03-25 11:09:57 +0800

    feat(install): zplug support

---
+++ zplug.sh
@@ -0,0 +1,21 @@
+version=$(grep -Eho "version = \"\d+\.\d+\.\d+" Cargo.toml | sed "s/version = \"//")
+
+if [ $(uname) == "Darwin" ]
+then
+    platform="macos"
+else
+    platform="linux"
+fi
+
+url="https://github.com/g-plane/zsh-yarn-autocompletions/releases/download/v$version/yarn-autocompletions_v${version}_$platform.tar.gz"
+
+if [ $(hash wget 2>/dev/null) ]
+then
+    wget $url > tarball.tar.gz
+else
+    curl -fsSL $url > tarball.tar.gz
+fi
+
+tar -zxf tarball.tar.gz ./ ./yarn-autocompletions
+
+rm tarball.tar.gz
