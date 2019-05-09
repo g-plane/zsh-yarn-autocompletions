12d2379ff5e38acce556af77588b0649d4fcf825
Pig Fang <g-plane@hotmail.com>
2019-05-09 23:14:49 +0800

    refactor

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod deps;
 fn main() {
     let args: Vec<String> = env::args().collect();
     match args[1].as_str() {
-        "scripts" => print!("{}", scripts::fetch_npm_scripts()),
+        "scripts" => print!("{}", scripts::fetch_npm_scripts().unwrap_or(String::new())),
         "add" => println!("{}", deps::return_dependencies(None)),
         "add-dev" => println!("{}", deps::return_dev_dependencies(None)),
         "remove" => print!("{}", deps::fetch_installed_packages()),
--- src/scripts.rs
+++ src/scripts.rs
@@ -1,49 +1,33 @@
 use serde::Deserialize;
+use std::collections::HashMap;
 use std::env;
 use std::fs::File;
-use std::collections::HashMap;
 
 #[derive(Deserialize)]
 struct Pkg {
     scripts: Option<HashMap<String, String>>,
 }
 
-pub fn fetch_npm_scripts() -> String {
-    let mut path = String::new();
-    match env::var("PWD") {
-        Ok(pwd) => {
-            path.push_str(&pwd);
-            path.push_str("/package.json");
-        }
-        Err(_) => {
-            return String::new();
-        }
-    }
+pub fn fetch_npm_scripts() -> std::io::Result<String> {
+    let mut path = env::current_dir().unwrap_or(std::path::PathBuf::from("."));
+    path.push("package.json");
+    let file = File::open(path)?;
+    let package: Pkg = serde_json::from_reader(file)?;
 
-    let file = File::open(path);
-    if let Err(_) = file {
-        return String::new();
-    }
-
-    let package: Result<Pkg, _> = serde_json::from_reader(file.unwrap());
-    if let Err(_) = package {
-        return String::new();
-    }
-    let package = package.unwrap();
-
-    match package.scripts {
+    let scripts = match package.scripts {
         Some(scripts) => scripts
             .keys()
             .map(|script| script.to_string())
-            .collect::<Vec<String>>()
+            .collect::<Vec<_>>()
             .join("\n"),
         None => String::new(),
-    }
+    };
+    Ok(scripts)
 }
 
 #[test]
 fn test_fetch_scripts() {
-    let output = fetch_npm_scripts();
+    let output = fetch_npm_scripts().unwrap();
     let output = output.trim();
     let mut scripts: Vec<&str> = output.split('\n').collect();
     scripts.sort();
