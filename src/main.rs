6b18eeb3e9a272095b25866360a0e477cd7ca1e0
Pig Fang <g-plane@hotmail.com>
2020-02-28 18:24:47 +0800

    remove unnecessary allocation

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,21 @@ mod scripts;
 
 fn main() {
     let mut args = std::env::args();
-    match args.next().unwrap_or(String::new()).as_str() {
-        "scripts" => print!("{}", scripts::fetch_npm_scripts().unwrap_or(String::new())),
+    match args.next().unwrap_or_default().as_str() {
+        "scripts" => print!("{}", scripts::fetch_npm_scripts().unwrap_or_default()),
         "add" => println!(
             "{}",
-            deps::return_dependencies(None).unwrap_or(String::new())
+            deps::return_dependencies(None).unwrap_or_default()
         ),
         "add-dev" => println!(
             "{}",
-            deps::return_dev_dependencies(None).unwrap_or(String::new())
+            deps::return_dev_dependencies(None).unwrap_or_default()
         ),
         "remove" => print!(
             "{}",
-            deps::fetch_installed_packages().unwrap_or(String::new())
+            deps::fetch_installed_packages().unwrap_or_default()
         ),
-        "why" => print!("{}", deps::list_node_modules().unwrap_or(String::new())),
+        "why" => print!("{}", deps::list_node_modules().unwrap_or_default()),
         _ => (),
     };
 }
